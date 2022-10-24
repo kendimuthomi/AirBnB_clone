c5e64a569f7fa0deaa25cde3ebc957855b3bcf30
Vashow <muriukiaw1@gmail.com>
2022-10-24 17:07:19 +0000

    Fixing unittest errors

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,6 +2,9 @@
 """ This provides automated tests for the class BaseModel """
 from models.base_model import BaseModel
 import unittest
+import uuid
+import datetime
+
 
 class BaseModelTestCase(unittest.TestCase):
     """The unittests of the basemodel"""
@@ -11,9 +14,9 @@ class BaseModelTestCase(unittest.TestCase):
 
         my_model = BaseModel()
         self.assertIsInstance(my_model, BaseModel)
-        self.assertIsInstance(my_model.created_at, datetime)
-        self.assertIsInstance(my_model.updated_at, datetime)
-        self.assertIsInstance(my_model.id, uuid)
+        self.assertIsInstance(my_model.created_at, datetime.datetime)
+        self.assertIsInstance(my_model.updated_at, datetime.datetime)
+        self.assertIsInstance(my_model.id, uuid.UUID)
 
 
     def testSave(self):
@@ -22,7 +25,7 @@ class BaseModelTestCase(unittest.TestCase):
         my_model = BaseModel()
         initial_datetime = my_model.updated_at
         my_model.save()
-        self.assertIsInstance(my_model.updated_at, datetime)
+        self.assertIsInstance(my_model.updated_at, datetime.datetime)
         self.assertIsNot(my_model.updated_at, initial_datetime)
 
     def testToDict(self):
@@ -30,8 +33,15 @@ class BaseModelTestCase(unittest.TestCase):
 
         my_model = BaseModel()
         my_model_json = my_model.to_dict()
-        self.assertIn(my_model_json.keys(), my_model.to_dict().keys())
-        self.assertIn(my_model_json.values(), my_mode.to_dict().values())
+        for key in my_model_json.keys():
+            if (key != '__class__'):
+                self.assertIn(key, my_model.__dict__.keys())
+
+        for key, value in my_model_json.items():
+            if (key != 'created_at' and key != 'updated_at'\
+                    and key != '__class__'):
+                self.assertIn(value, my_model.__dict__.values())
+
         self.assertIn('__class__', my_model_json.keys())
         self.assertIsInstance(my_model_json['created_at'], str)
         self.assertIsInstance(my_model_json['updated_at'], str)
