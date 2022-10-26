e3ea3706a8fc4d51dd217e11b186abf4219e73f9
Vashow <muriukiaw1@gmail.com>
2022-10-26 16:49:30 +0000

    Doing some testcases

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" Contains unittests for file_storage.py"""
+import unittest
+from models import storage
+from models.base_model import BaseModel
+import datetime
+import json
+
+
+class FileStorageTestCase(unittest.TestCase):
+    """Defiinition of the class for the unittests"""
+
+    def testAll(self):
+        """
+        Testing the method all() from FileStorage
+        Initially when there is no file.json
+
+        """
+        all_objs = storage.all()
+        self.assertEqual(all_objs, {})
+        new_model_1 = BaseModel()
+        new_model_2 = BaseModel()
+        new_model_3 = BaseModel()
+
+        all_objs = storage.all()
+        objs = []
+        for obj in all_objs.values():
+            objs.append(obj)
+
+        self.assertIn(new_model_1, objs)
+        self.assertIn(new_model_2, objs)
+        self.assertIn(new_model_3, objs)
+
+    def testNew(self):
+        """Testing the method new()"""
+        new_model = BaseModel()
+        all_objs = storage.all()
+        obj_values = []
+        for obj in all_objs.values():
+            for value in obj.to_dict().values():
+                obj_values.append(value)
+
+        self.assertIsInstance(new_model, BaseModel)
+        self.assertIn(new_model.id, obj_values)
+        self.assertIn(new_model.created_at.isoformat(), obj_values)
+        self.assertIn(new_model.updated_at.isoformat(), obj_values)
+
+    def testSave(self):
+        """Testing the methon save()"""
+        model = BaseModel()
+        model_1 = BaseModel()
+        model_2 = BaseModel()
+        storage.save()
+        with open("file.json", encoding="utf-8") as f:
+            all_dicts = json.load(f)
+
+        objects_values = []
+        for obj in all_dicts.values():
+            for values in obj.values():
+                objects_values.append(values)
+
+        self.assertIn(model.id, objects_values)
+        self.assertIn(model_1.id, objects_values)
+        self.assertIn(model_2.id, objects_values)
+
