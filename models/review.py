d193328b1a7061eb36ab24af5b2d4c2443b474b0
kendimuthomi <kendijackline42@gmail.com>
2022-10-27 13:34:29 +0000

    complying to pycodestyle

--- models/review.py
+++ models/review.py
@@ -2,6 +2,7 @@
 from models.base_model import BaseModel
 """Defines a class review"""
 
+
 class Review(BaseModel):
     """
     Representation of the
@@ -12,7 +13,7 @@ class Review(BaseModel):
     place_id: Place.id
     user_id: User.id
     """
-    
+
     text = ""
     place_id = ""
     user_id = ""
