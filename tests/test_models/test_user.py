8e6cff9b8bbbf3bae1cbdd24ed7bda14f73f1ea1
kendimuthomi <kendijackline42@gmail.com>
2022-10-27 13:48:53 +0000

    complying to pycodestyle

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,12 +8,12 @@ class UserTestCase(unittest.TestCase):
     """The unittest for the User class"""
 
     def test_class_attrs(self):
-       """Test the class attributes"""
-       u = User()
-       self.assertIs(type(u.email), str)
-       self.assertIs(type(u.last_name), str)
-       self.assertIs(type(u.first_name), str)
-       self.assertIs(type(u.password), str)
+        """Test the class attributes"""
+        u = User()
+        self.assertIs(type(u.email), str)
+        self.assertIs(type(u.last_name), str)
+        self.assertIs(type(u.first_name), str)
+        self.assertIs(type(u.password), str)
 
     def test_attrs_are_class_attrs(self):
         """Test if the attributes are class attributes"""
