201069f24f2defb2b0d880324a37b97117d2596c
Vashow <muriukiaw1@gmail.com>
2022-10-27 13:00:10 +0000

    Sth just work

--- console.py
+++ console.py
@@ -188,5 +188,6 @@ class HBNBCommand(cmd.Cmd):
                 setattr(all_obs[name], attribute, eval(att_val))
                 HBNBCommand.new_storage.save()
 
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
