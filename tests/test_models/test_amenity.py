59e6d99f0bcb3240f9641e6accc837f0162dac8f
kendimuthomi <kendijackline42@gmail.com>
2022-10-28 13:14:01 +0000

    Updating class tests

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+
+class AmenityTestCase(unittest.TestCase):
+    """The unittest for the subclass Amenity"""
+
+    def test_class_attributes(self):
+        a = Amenity()
+        self.assertIs(type(name), str)
+
+    def test_attr_are_cls_atrr(self):
+        """tests that these are class attributes"""
+        a = Amenity()
+        self.assertTrue(hasattr(Amenity, "name"))
+
+    def test_if_basemodel_subclass(self):
+        a = Amenity()
+        self.assertTrue(issubclass(type(a), BaseModel))
