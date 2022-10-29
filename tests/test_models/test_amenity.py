5eb2cceefe1ad092b5b1d8528fd05c276545916a
kendimuthomi <kendijackline42@gmail.com>
2022-10-29 09:24:49 +0000

    More tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,6 +2,7 @@
 import unittest
 from models.base_model import BaseModel
 from models.amenity import Amenity
+from models import storage
 
 
 class AmenityTestCase(unittest.TestCase):
@@ -10,16 +11,24 @@ class AmenityTestCase(unittest.TestCase):
         self.a = Amenity()
 
     def test_class_attributes(self):
-<<<<<<< HEAD
+        """tests the class attributes"""
         self.assertIs(type(self.a.name), str)
-=======
-        a = Amenity()
-        self.assertIs(type(a.name), str)
->>>>>>> b3c2151c8997211a4d0c3f3d7e81fa0693c1a209
 
     def test_attr_are_cls_atrr(self):
         """tests that these are class attributes"""
         self.assertTrue(hasattr(self.a, "name"))
 
     def test_if_basemodel_subclass(self):
+        """test if amenity is a subclass of basemodel"""
         self.assertTrue(issubclass(type(self.a), BaseModel))
+
+    def test_instances_stored_in_objects(self):
+        """tests if the instances of amenity are stored in __objects"""
+        self.assertIn(self.a, storage.all().values())
+
+    def test_two_amenities_have_unique_ids(self):
+        """tests if two instances of the subclass amenity have unique ids"""
+        a1 = Amenity()
+        a2 = Amenity()
+        self.assertNotEqual(a1.id, a2.id)
+
